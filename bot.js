203febd3e2fd863d28e8505e7a901a0982fcc632
yafael <yafael@users.noreply.github.com>
2017-02-07 17:37:34 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function respond() {
   
 if(String(request).length > 0) {
     this.res.writeHead(200);
-    postMessage(this.req);
+    postMessage(String(this.req));
     this.res.end();
   }
   else if(request.text && botRegexSlut.test(request.text)) {
